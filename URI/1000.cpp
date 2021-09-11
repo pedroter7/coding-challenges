1d1ae1647cf9119cea821271a28212063607998e
Pedro Tersetti Freidinger <pedrotersetti3@gmail.com>
2021-09-11 20:57:22 -0300

    Rename 1001->1000

---
+++ URI/1000.cpp
@@ -0,0 +1,15 @@
+/*
+* URI 1000 - Hello World!
+* Solution by Pedro T Freidinger (github.com/pedroter7)
+*/
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    cout << "Hello World!" << endl;
+ 
+    return 0;
+}
